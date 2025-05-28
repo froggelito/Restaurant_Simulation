046844039dd9f9951797b56c1e72bdfeecfa7dd6
froggelito <06rohshe@gafe.molndal.se>
2025-05-28 19:52:28 +0200

    Delete TableSubscriber.java

--- TableSubscriber.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface TableSubscriber {
-
-    public void receiverOrder (int x, int y, int tableNumber, String appetizer, String mainCourse, String dessert);
-
-}
