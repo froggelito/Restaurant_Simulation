0f242812f762bd267d480aece0d9098b8dd18adc
froggelito <06rohshe@gafe.molndal.se>
2025-05-28 20:00:33 +0200

    Update Waiter.java

--- Waiter.java
+++ Waiter.java
@@ -1,53 +1,90 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
-public class Waiter extends Staff implements TableSubscriber {
+public class Waiter extends Person {
 
-    private int diameter = 50;
-    HashMap<Integer, String> appetizerMenu;
-    HashMap<Integer, String> mainCourseMenu;
-    HashMap<Integer, String> dessertMenu;
+    private ArrayList<ArrayList<String>> tableOrder;
+    private Table assignedTable;
+    private MasterChef masterChef;
+    private int visits;
 
-    public Waiter (int x, int y){
+    public Waiter(int x, int y, Table assignedTable, MasterChef masterChef) {
         super(x, y);
-        appetizerMenu = new HashMap<>();
-        mainCourseMenu = new HashMap<>();
-        dessertMenu = new HashMap<>();
+        tableOrder = new ArrayList<>();
+        this.assignedTable = assignedTable;
+        this.masterChef = masterChef;
+        visits = 0;
     }
 
-
-    public void receiverOrder(int x, int y, int tableNumber, String appetizer, String mainCourse, String dessert) {
-        appetizerMenu.put(tableNumber, appetizer);
-        mainCourseMenu.put(tableNumber, mainCourse);
-        dessertMenu.put(tableNumber, dessert);
+    public ArrayList<String> getTableOrder (int course) {
+        return tableOrder.get(course);
     }
 
-    public void walkToTable (Table table) {
-
+    public Table getAssignedTable () {
+        return assignedTable;
     }
 
-    public void walkToKitchenMaster () {
-
+    /**
+     * Retrieves orders from the assigned table and adds to the waiter's list.
+     */
+    public void takeOrder() {
+        //visits++;
+        tableOrder.add(assignedTable.getTableOrders().get(visits));
     }
 
-    public int getX() {
-        return x;
+    public ArrayList<String> serveOrder () {
+        return masterChef.giveOutgoingOrders();
     }
 
-    public int getY() {
-        return y;
+    public int getVisits () {
+        return visits;
     }
 
-    public int getDiameter() {
-        return diameter;
+    /**
+     * Sets destination to the assigned table's coordinates and begins walking.
+     */
+    public void walkToTable() {
+        destinationX = assignedTable.getX() - assignedTable.getDiameter()/2;
+        destinationY = assignedTable.getY();
+        this.walk();
     }
 
-    public String getOrder () {
-        return "hej";
+    /**
+     * Sets destination to the MasterChef and walks after a delay.
+     */
+    public void walkToMasterChef() {
+        startTimer();
+
+        long timer = System.currentTimeMillis();
+        if (timer - startTime > 2000) {
+            destinationX = masterChef.getX() + masterChef.getDiameter();
+            destinationY = masterChef.getY();
+            this.walk();
+        }
     }
 
+    /**
+     * Updates the waiter behavior based on current state:
+     */
     public void update () {
+        if (this.state == States.WALKING) {
+            this.walkToTable();
+            if (arrived){
+                this.setState(States.COMMUNICATING);
+                this.takeOrder();
+                arrived = false;
+            }
+        }
 
+        else if (this.state == States.COMMUNICATING) {
+            this.walkToMasterChef();
+            if (arrived){
+                this.setState(States.IDLE);
+                masterChef.receiveOrder(this);
+                arrived = false;
+            }
+        }
+        //Masterchef state för att få beställningen
     }
 }
